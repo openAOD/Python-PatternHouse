15045b4f3e75edf1264d02fdd0c252b11b78ada3
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-09-14 11:26:57 +0530

    Done Python Numeric Pattern-2

---
+++ Numeric Patterns/Source Code/numeric_pattern2.py
@@ -0,0 +1,15 @@
+# Numeric Pattern 2
+
+"""
+Desired Output:
+
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+"""
+
+pattern = " ".join(map(str, list(range(1, 6))))
+for _ in range(5):
+    print(pattern)
