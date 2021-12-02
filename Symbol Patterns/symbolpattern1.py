c36f95768eaf2d3cbc93a72c3c627dce2fc366bd
Daksh P. Jain <jaindaksh42@gmail.com>
2021-12-02 15:09:32 +0530

    Create symbolpattern1.py

---
+++ Symbol Patterns/symbolpattern1.py
@@ -0,0 +1,17 @@
+"""
+In this question, we simply have to print a n*n grid
+We can do this by using a single for loop
+"""
+size = 5 # n*n grid
+for i in range(size):
+    print("*"*size)
+
+"""
+OUTPUT:
+
+*****
+*****
+*****
+*****
+*****
+"""
