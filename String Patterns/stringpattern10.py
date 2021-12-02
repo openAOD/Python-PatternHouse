87c5d516874f155ee81ad475d420de4603215046
Yashesvinee V <76458668+Yashesvinee@users.noreply.github.com>
2021-12-02 20:02:41 +0530

    Update stringpattern10.py

--- String Patterns/stringpattern10.py
+++ String Patterns/stringpattern10.py
@@ -4,7 +4,7 @@ print('Enter the number of lines')
 lines = int(input())
 index = 0
 for i in range (lines) :
-  for j in range (lines,i,-1) :
+  for j in range (lines-1, i, -1) :
     print(' ', end =' ')
   for k in range (i+1) :
     try:
