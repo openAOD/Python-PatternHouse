14273a380728d51fa8be9e1c830bf2ef38f71734
Yashesvinee V <76458668+Yashesvinee@users.noreply.github.com>
2021-12-02 19:41:07 +0530

    Update stringpattern7.py

--- String Patterns/stringpattern7.py
+++ String Patterns/stringpattern7.py
@@ -8,7 +8,7 @@ for i in range (math.ceil(len(pattern_string)/num)) :
   for j in range (num):
     try:
       print(pattern_string[index], end=' ')
-      index+ = 1
+      index+= 1
     except:
       print(' ', end ='')  
   print(end ='\n')
