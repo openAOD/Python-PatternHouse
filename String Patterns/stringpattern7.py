52622f063e82999573b0c96bccb4065161846276
Yashesvinee V <76458668+Yashesvinee@users.noreply.github.com>
2021-12-02 19:33:27 +0530

    Create stringpattern7.py

---
+++ String Patterns/stringpattern7.py
@@ -0,0 +1,14 @@
+import math 
+print('Enter a string')
+pattern_string = input()
+print('Enter the number of letters to be printed in each line')
+num = int(input())
+index = 0
+for i in range (math.ceil(len(pattern_string)/num)) :
+  for j in range (num):
+    try:
+      print(pattern_string[index], end=' ')
+      index+ = 1
+    except:
+      print(' ', end ='')  
+  print(end ='\n')
