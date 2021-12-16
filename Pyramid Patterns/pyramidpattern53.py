a5d2c35b3f1c6830763c7304bed88113e0320315
Lovish Prabhakar <2020010006574@gndu.ac.in>
2021-12-16 23:12:41 +0530

    Create pyramidpattern53.py

---
+++ Pyramid Patterns/pyramidpattern53.py
@@ -0,0 +1,16 @@
+n = 4
+for i in range(n+1):
+    print(" "*(2*(n-i))+"* "*i)
+for i in range(n-1, 0, -1):
+    print(" "*(2*(n-i))+"* "*i)
+
+"""
+OUTPUT:
+      * 
+    * *
+  * * *
+* * * *
+  * * *
+    * *
+      *
+"""
