1f1c3eacc83610b1c00e8d64e699cc004197b411
Lovish Prabhakar <2020010006574@gndu.ac.in>
2021-12-16 19:39:07 +0530

    Create numericpattern34.py

---
+++ Numeric Patterns/numericpattern34.py
@@ -0,0 +1,22 @@
+start = 1
+stop = 2
+current_num = stop
+for row in range(2, 6):
+    for col in range(start, stop):
+        current_num -= 1
+        print(current_num, end=' ')
+    print("")
+    start = stop
+    stop += row
+    current_num = stop
+
+'''
+
+Output:
+
+1 
+3 2
+6 5 4
+10 9 8 7
+
+'''
