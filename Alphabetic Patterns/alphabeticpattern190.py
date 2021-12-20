5ffbd853799ca74a94a46f3c8378e57e10bb1e00
anshyyy <anshuman9998@gmail.com>
2021-12-20 18:57:05 +0530

    Solved isssue #308

--- Alphabetic Patterns/alphabeticpattern190.py
+++ Alphabetic Patterns/alphabeticpattern190.py
@@ -3,7 +3,7 @@ counter = 0
 for i in range(0,height) :
         for j in range(0,height+1) :
             if ( j == counter or j == height - counter and i <= height // 2 ):
-                print("Y ",end="")
+                print("Y",end="")
             else :
                 print(end=" ")
         print()
