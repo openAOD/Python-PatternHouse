ab622ed515f0725a1f850e1b3e71852868564898
anshyyy <anshuman9998@gmail.com>
2021-12-30 11:44:51 +0530

    Solved issue #278

---
+++ Alphabetic Patterns/alphabeticpattern160.py
@@ -0,0 +1,14 @@
+height = 5
+for i in range(0, height):
+    if (i != 0 and i != height - 1):
+        print("*", end="")
+    else:
+        print(end=" ")
+    for j in range(0, height):
+        if (((i == height - 1) and j >= 0 and j < height - 1)):
+            print("*", end="")
+        elif (j == height - 1 and i != 0 and i != height - 1):
+            print("*", end="")
+        else:
+            print(end=" ")
+    print()
