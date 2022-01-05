86aa9079aca521170a80183078a757f3e353cb55
anshyyy <anshuman9998@gmail.com>
2022-01-05 14:24:23 +0530

    solved issue #829

---
+++ Symbol Patterns/symbolpattern192.py
@@ -0,0 +1,9 @@
+height = 5
+for i in range(0,height) :
+        print("*",end="")
+        for j in range(0,height+1) :
+            if ( i == height - 1 ):
+                print("*",end="")
+            else :
+                print(end=" ")
+        print()
