a72067a897d2183f265259a7f39c41f21089a21a
anshyyy <anshuman9998@gmail.com>
2022-01-05 14:20:48 +0530

    Solved issue #827

--- Symbol Patterns/symbolpattern190.py
+++ Symbol Patterns/symbolpattern190.py
@@ -2,7 +2,7 @@ height = 5
 for i in range(0,height) :
         for j in range(0,height) :
             if(i==0):
-                 print("*")
+                 print("*",end="")
             if ( i == height - 1 and (j > 0 and j < height - 1) ):
                 print("*",end="")
             elif ( (j == height - 1 and i != height - 1) or (i > (height // 2) - 1 and j == 0 and i != height - 1) ) :
