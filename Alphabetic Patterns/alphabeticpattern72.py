def08262c9ba28f9d0b20ffb1067e6fad5ecf4d7
Eshan Gupta <eshan1925@gmail.com>
2022-01-17 23:35:23 +0530

    Create alphabeticpattern72.py

---
+++ Alphabetic Patterns/alphabeticpattern72.py
@@ -0,0 +1,11 @@
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print("Enter the no of rows: ")
+n = int(input())
+
+for i in range(0, n):
+    for j in range(0,i+1):
+        if(i%2==0):
+            print(alpha[i],end=" ")
+        else:
+            print(i+1,end=" ")
+    print()
