042a4f42ea6b4b7c780d86392722e1a4b4ce52ce
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 14:07:43 +0530

    Create numericpattern28.py

---
+++ Numeric Patterns/numericpattern28.py
@@ -0,0 +1,17 @@
+height = int(input())
+
+for i in range(height,0,-1) :
+
+    for j in range(height, i-1,-1):
+        print(j,end = " ")
+        
+    print()
+
+// Sample Input :- 5
+// output :-
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2 
+// 5 4 3 2 1
+
