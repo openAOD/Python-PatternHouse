1294bfc58917411e989c689999b70cbddc5e4d22
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:39:57 +0530

    Update numericpattern35.py

--- Numeric Patterns/numericpattern35.py
+++ Numeric Patterns/numericpattern35.py
@@ -14,11 +14,11 @@ for i in range(1,height+1) :
         
     print()
 
-// Sample Input :- 5
-// Output :-
-//  1 
-//  3  5 
-//  7  9 11 
-// 13 15 17 19 
-// 21 23 25 27 29 
+# Sample Input :- 5
+# Output :-
+#  1 
+#  3  5 
+#  7  9 11 
+# 13 15 17 19 
+# 21 23 25 27 29 
 
