f23f467b8a23f534e9d67e1f2f8de8e83eaa13e3
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:33:25 +0530

    Update numericpattern21.py

--- Numeric Patterns/numericpattern21.py
+++ Numeric Patterns/numericpattern21.py
@@ -11,10 +11,10 @@ for i in range(0,height) :
         
     print()
     
-// Sample Input :- 5
-// Output :- 
-// 0 0 0 0 0
-// 1 1 1 1 1
-// 0 0 0 0 0
-// 1 1 1 1 1
-// 0 0 0 0 0 
+# Sample Input :- 5
+# Output :- 
+# 0 0 0 0 0
+# 1 1 1 1 1
+# 0 0 0 0 0
+# 1 1 1 1 1
+# 0 0 0 0 0 
