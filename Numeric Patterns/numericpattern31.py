d2aefd92ccef5b49dd2cdebba809bcb2a760c660
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 14:54:42 +0530

    Create numericpattern31.py

---
+++ Numeric Patterns/numericpattern31.py
@@ -0,0 +1,21 @@
+height = int(input())
+m = 1
+
+for i in range(1,height+1) :
+
+    for j in range(1, i+1):
+        if(m <= 9):
+            print("",m,end = " ")
+        else:
+            print(m,end = " ")
+        m+=1
+        
+    print()
+
+// Sample Input :- 5
+// Output :- 
+//  1
+//  2  3 
+//  4  5  6
+//  7  8  9 10
+// 11 12 13 14 15
