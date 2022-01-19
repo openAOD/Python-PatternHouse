451a6f84f11c87c14dbf0b30bf015a0742f45c68
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:35:40 +0530

    Update numericpattern27.py

--- Numeric Patterns/numericpattern27.py
+++ Numeric Patterns/numericpattern27.py
@@ -9,10 +9,10 @@ for i in range(1,height+1) :
         
     print()
 
-// Sample Input :- 5
-// Output :-
-// 5
-// 4 4
-// 3 3 3 
-// 2 2 2 2
-// 1 1 1 1 1
+# Sample Input :- 5
+# Output :-
+# 5
+# 4 4
+# 3 3 3 
+# 2 2 2 2
+# 1 1 1 1 1
