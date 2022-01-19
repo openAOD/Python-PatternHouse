4aeb98ccb920b69b97aed47c91d0c6a26e19bd12
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:47:11 +0530

    Update numericpattern306.py

--- Numeric Patterns/numericpattern306.py
+++ Numeric Patterns/numericpattern306.py
@@ -8,7 +8,7 @@ for i in range(1,height+1) :
         c = chr(i+64)
         print(c,end = " ")
         
-    for j in range(1, +1):
+    for j in range(1, i+1):
         print(i,end = " ")
         
     print()
