2aaef304b730e967ea887ff4150043bb500c6afb
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:36:57 +0530

    Update numericpattern31.py

--- Numeric Patterns/numericpattern31.py
+++ Numeric Patterns/numericpattern31.py
@@ -4,6 +4,7 @@ m = 1
 for i in range(1,height+1) :
 
     for j in range(1, i+1):
+        
         if(m <= 9):
             print("",m,end = " ")
         else:
@@ -12,10 +13,10 @@ for i in range(1,height+1) :
         
     print()
 
-// Sample Input :- 5
-// Output :- 
-//  1
-//  2  3 
-//  4  5  6
-//  7  8  9 10
-// 11 12 13 14 15
+# Sample Input :- 5
+# Output :- 
+#  1
+#  2  3 
+#  4  5  6
+#  7  8  9 10
+# 11 12 13 14 15
