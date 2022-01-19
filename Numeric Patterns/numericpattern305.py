eeb78a048c9bc85101afad58f045111ab0907684
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:30:21 +0530

    Update numericpattern305.py

--- Numeric Patterns/numericpattern305.py
+++ Numeric Patterns/numericpattern305.py
@@ -2,6 +2,7 @@ height = int(input())
 x = 1
 
 for i in range(1,height+1) :
+    
     for j in range(1, height-i+1):
         print(end = "  ")
     
@@ -15,10 +16,10 @@ for i in range(1,height+1) :
     print()
     x = x + 2
     
-// Sample Input :- 5
-// Output :-
-//          1 A
-//        2 3 B C
-//      3 4 5 C D E
-//    4 5 6 7 D E F G
-//  5 6 7 8 9 E F G H I
+# Sample Input :- 5
+# Output :-
+#          1 A
+#        2 3 B C
+#      3 4 5 C D E
+#    4 5 6 7 D E F G
+#  5 6 7 8 9 E F G H I
