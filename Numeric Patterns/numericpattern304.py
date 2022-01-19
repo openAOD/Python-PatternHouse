ffec2e77cf25c67f4afe9bba57e1575399cdd1f8
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:22:52 +0530

    Create numericpattern304.py

---
+++ Numeric Patterns/numericpattern304.py
@@ -0,0 +1,22 @@
+height = int(input())
+
+for i in range(1,height+1) :
+    for j in range(1, height-i+1):
+        print(end = "  ")
+    
+    for j in range(1, i+1):
+        print(j,end = " ")
+        
+    for j in range(1, i+1):
+        c = chr(j+64)
+        print(c,end = " ")
+        
+    print()
+    
+// Sample Input :- 5
+// Output :- 5
+//         1 A
+//       1 2 A B
+//     1 2 3 A B C
+//   1 2 3 4 A B C D
+// 1 2 3 4 5 A B C D E
