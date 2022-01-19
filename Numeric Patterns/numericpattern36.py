b20753d2d0d5657aa6e88961bd16bad6881e63f8
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-19 15:40:46 +0530

    Update numericpattern36.py

--- Numeric Patterns/numericpattern36.py
+++ Numeric Patterns/numericpattern36.py
@@ -15,11 +15,11 @@ for i in range(1,height+1) :
         
     print()
 
-// Sample Input :- 5
-// Output :-
-//  2
-//  4  6
-//  8 10 12
-// 14 16 18 20
-// 22 24 26 28 30
+# Sample Input :- 5
+# Output :-
+#  2
+#  4  6
+#  8 10 12
+# 14 16 18 20
+# 22 24 26 28 30
 
