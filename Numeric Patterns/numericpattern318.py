d796eb29a00c8bf84e06fee9399a871f2a916903
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-20 13:06:23 +0530

    Create numericpattern318.py

---
+++ Numeric Patterns/numericpattern318.py
@@ -0,0 +1,21 @@
+height = int(input())
+
+for i in range(0,height):
+
+    for j in range(0,height):
+    
+        if(i == 0 or i == height//2 or i == height-1 or j == height-1):
+            print("*",end=" ")
+        elif(i < height//2 and j == 0):
+            print("*",end=" ")
+        else:
+            print(end="  ")
+    print()
+    
+# Sample Input :- 5
+# Output :-
+# * * * * *
+# *       *
+# * * * * *
+#         *
+# * * * * *
