a34aed54e4f3a7011278b0494b55dc08ce87f6fa
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-23 12:34:36 +0530

    Create alphabeticpattern99.py

---
+++ Alphabetic Patterns/alphabeticpattern99.py
@@ -0,0 +1,22 @@
+height = int(input())
+
+for i in range(1, height+1):
+
+    for j in range(1,height+1):
+    
+        if(i == j or i == height-j+1):
+            c = chr(height - i + 65)
+            print(c,end=" ")
+        
+        else:
+            print(end = "  ")
+            
+    print()
+    
+# Sample Input :- 5
+# Output :- 
+# E       E
+#   D   D
+#     C
+#   B   B
+# A       A
