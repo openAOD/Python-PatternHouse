ce1478ea774c53b86612409939f4303e0a79de53
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-23 12:13:14 +0530

    Create pyramidpattern64.py

---
+++ Pyramid Patterns/pyramidpattern64.py
@@ -0,0 +1,25 @@
+height = int(input())
+
+for i in range(height, 0, -1):
+
+    for j in range(1,height-i+1):
+    
+        print(end=" ")
+        
+    for j in range(1,i+1):
+    
+        if(i%2 == 0):  
+            print("*",end=" ")
+            
+        else:
+            print("#",end=" ")
+            
+    print()
+    
+# Sample Input :- 5
+# Output :-
+# # # # # #
+#  * * * *
+#   # # #
+#    * *
+#     #
