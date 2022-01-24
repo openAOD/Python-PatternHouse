617e46ca4b239100a87358ccba784b83bc1c9874
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-24 16:06:49 +0530

    Create symbolpattern86.py

---
+++ Symbol Patterns/symbolpattern86.py
@@ -0,0 +1,26 @@
+height = int(input())
+x = height
+y = height
+
+for i in range(1,height+1):
+
+    for j in range(1,height*2+1):
+    
+        if(j == x or j == y):
+            print("*",end=" ")
+        
+        else:
+            print(end="  ")
+        
+    x -= 1
+    y += 1
+        
+    print()
+    
+# Sample Input :- 5
+# Output :-
+#           *
+#         *   *
+#      *         *
+#    *             *
+# *                  *
