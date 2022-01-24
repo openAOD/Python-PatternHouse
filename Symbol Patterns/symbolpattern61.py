4bccddb224ffa91cf7c130bf58007b9f67d34eee
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-24 14:03:26 +0530

    Create symbolpattern61.py

---
+++ Symbol Patterns/symbolpattern61.py
@@ -0,0 +1,25 @@
+height = int(input())
+
+for i in range(1,height+1):
+
+    for j in range(1,height-i+1):
+    
+        print(end=" ")
+    
+    for j in range(1,i+1):
+    
+        if(i%2 == 0):
+            print("*",end=" ")
+        
+        else:
+            print("#",end=" ")
+                    
+    print()
+    
+# Sample Input :- 5
+# Output :-
+#     #
+#    * *
+#   # # #
+#  * * * *
+# # # # # #
