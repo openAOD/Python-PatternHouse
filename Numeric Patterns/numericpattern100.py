ed97ead6f2f8e200cb87a35dc337f9df1f62bf7c
Eshan Gupta <eshan1925@gmail.com>
2022-01-28 19:47:40 +0530

    Create numericpattern100.py

---
+++ Numeric Patterns/numericpattern100.py
@@ -0,0 +1,27 @@
+
+def pattern(n):
+    
+    for i in range(n):
+        count = 2*i+1
+        for j in range(n - i - 1):
+            print(' ', end='')
+        for k in range(2 * i + 1):
+            print(count, end='')
+            count = count - 1
+        print()
+        
+
+
+print("Enter the number of rows: ")
+n = int(input())
+pattern(n)
+
+# OUTPUT
+
+# Enter the number of rows: 
+# 5
+#     1
+#    321
+#   54321
+#  7654321
+# 987654321
