499e0964cd03d4d35a0846c3a28a394a8389fc9f
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-31 14:19:25 +0530

    Create symbolpattern89.py

---
+++ Symbol Patterns/symbolpattern89.py
@@ -0,0 +1,24 @@
+[14:17, 31/01/2022] Mahak Garg: height = int(input())
+
+for i in range(1,height+1):
+
+    for j in range(1,height//2+2):
+    
+        if(i+j == height//2+2 or i-j == height//2):
+            print("*",end=" ")
+        
+        else:
+            print(end="  ")
+        
+    print()
+    
+# Sample Input :- 7
+# Output :-
+#       *
+#     *
+#   *
+# *
+#   *
+#     *
+#       *
+
