6250bdab9c7003efadf764bb1c72b9715b8bfb78
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-02 14:34:31 +0530

    Create symbolpattern134.py

---
+++ Symbol Patterns/symbolpattern134.py
@@ -0,0 +1,23 @@
+height = int(input())
+
+for i in range(1,height+1):
+
+    for j in range(1,height+1):
+
+        if(i >= height-j+1 or i == j):
+            print("*",end=" ")
+
+        else:
+            print(end="  ")
+
+    print()
+    
+# Sample Input :- 7
+# Output :-
+# *           *
+#   *       * *
+#     *   * * *        
+#       * * * *
+#     * * * * *
+#   * * * * * *
+# * * * * * * *
