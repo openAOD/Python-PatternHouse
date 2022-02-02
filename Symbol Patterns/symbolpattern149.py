7cdf1d1d740e779354bff40b0f531735e3228bdc
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-02 13:18:58 +0530

    Create symbolpattern149.py

---
+++ Symbol Patterns/symbolpattern149.py
@@ -0,0 +1,25 @@
+height = int(input())
+x = height//2-1
+
+for i in range(0,height):
+    
+    for j in range(0, height):
+
+        if(i+j >= 3*height//2-1 or i+j <= x or i-j >= x or j-i >= x):
+            print("*",end=" ")
+
+        else:
+            print(end="  ")
+
+    print()
+    
+# Sample Input :- 8
+# Output :-
+# * * * * * * * *
+# * * *   * * * *
+# * *       * * *
+# *           * *
+# * *           *
+# * * *       * *
+# * * * *   * * *
+# * * * * * * * *
