a117a01e882f1d2c1713724e8f43c78fdf32ae75
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-08 17:18:46 +0530

    Create numericpattern279.py

---
+++ Numeric Patterns/numericpattern279.py
@@ -0,0 +1,42 @@
+height = int(input())
+x = 1
+sum = height*(height+1)
+
+for i in range(1,height+1):
+    y = sum - height + i
+
+    for j in range(1,height*2+1):
+        
+        if(j > 2*(i-1)):
+        
+            if(j <= (height*2)//2+i-1):
+            
+                if(x <= 9):
+                    print("",x,end=" ")
+
+                else:
+                    print(x,end=" ")
+                    
+                x += 1
+
+            else:
+                if(y <= 9):
+                    print("",y,end=" ")
+
+                else:
+                    print(y,end=" ")
+                    
+                y += 1
+                    
+        else:
+            print(" _",end=" ")
+
+    sum = sum - height + i - 1
+    print()
+    
+# Sample Input :- 4
+# Output :-
+# 1  2  3  4 17 18 19 20 
+# _  _  5  6  7 14 15 16 
+# _  _  _  _  8  9 12 13 
+# _  _  _  _  _  _ 10 11 
