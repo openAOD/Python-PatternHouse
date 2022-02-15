2aaf2e37eb462858de75d0928af49354a49b8ef3
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-15 13:08:17 +0530

    Create numericpattern271.py

---
+++ Numeric Patterns/numericpattern271.py
@@ -0,0 +1,22 @@
+height = int(input())
+no = int(input())
+
+for i in range(1,no+1):
+
+    for j in range(1,height+1):
+
+        for k in range(1,j+1):
+            print(j,end=" ")
+
+        print()
+
+# Sample Input :- 3
+#                 2
+# 1 
+# 2 2 
+# 3 3 3 
+# 1 
+# 2 2 
+# 3 3 3 
+
+        
