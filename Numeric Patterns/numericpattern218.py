2cfe246d253a7e2e03e4032b5c2475d71212493f
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-19 11:13:01 +0530

    Create numericpattern218.py

---
+++ Numeric Patterns/numericpattern218.py
@@ -0,0 +1,25 @@
+height = int(input()) 
+
+for i in range(height,0,-1):
+
+    for j in range(i,height):
+        print(end=" ")
+
+    for j in range(1,i+1):
+
+        if(i%2 != 0):
+            c = chr(j+64)
+            print(c,end=" ")
+
+        else:
+            print(j,end=" ")
+
+    print()
+    
+# Sample Input :- 5
+# Output :-
+# A B C D E 
+#  1 2 3 4 
+#   A B C 
+#    1 2 
+#     A 
