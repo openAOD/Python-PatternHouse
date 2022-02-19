ad8e5360a93ebfdf0de1fba0790227705b88fb04
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-19 13:15:04 +0530

    Create numericpattern183.py

---
+++ Numeric Patterns/numericpattern183.py
@@ -0,0 +1,21 @@
+height = int(input()) 
+
+for i in range(height,0,-1):
+
+    for j in range(1,i+1):
+
+        if(i%2 == 0):
+            print("*",end=" ")
+
+        else:
+            print(i,end=" ")
+
+    print()
+    
+# Sample Input :- 5
+# Output :-
+# 5 5 5 5 5 
+# * * * * 
+# 3 3 3 
+# * * 
+# 1 
