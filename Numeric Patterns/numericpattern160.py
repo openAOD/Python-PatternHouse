7c429db930437f31de29016afc908b0749be9b2e
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-24 17:18:36 +0530

    Create numericpattern160.py

---
+++ Numeric Patterns/numericpattern160.py
@@ -0,0 +1,22 @@
+height = int(input())
+x = 1
+
+for i in range(1,height+1):
+
+    for j in range(1,height+1):
+
+        if(i == height - j + 1) :
+            print("*",end=" ")
+
+        else :
+            print(j,end=" ")
+
+    print()
+    
+# Sample Input :- 5
+# Output :-
+# 1 2 3 4 * 
+# 1 2 3 * 5 
+# 1 2 * 4 5 
+# 1 * 3 4 5 
+# * 2 3 4 5 
