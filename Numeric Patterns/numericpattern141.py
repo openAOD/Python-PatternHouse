9bbf353629e5667edea82e7972559e074e4a7948
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-25 13:01:47 +0530

    Create numericpattern141.py

---
+++ Numeric Patterns/numericpattern141.py
@@ -0,0 +1,21 @@
+height = int(input())
+
+for i in range(1,height+1):
+
+    for j in range(1,height+1):
+        
+        if(j < height//2+1):
+            print(0,end=" ")
+
+        else:
+            print(1,end=" ")
+
+    print()
+    
+# Sample Input :- 5
+# Output :-
+# 0 0 1 1 1 
+# 0 0 1 1 1 
+# 0 0 1 1 1 
+# 0 0 1 1 1 
+# 0 0 1 1 1 
