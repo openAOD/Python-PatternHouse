cda69ee7a3e216eee58f932a8bce27c75f71a88b
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-25 14:11:12 +0530

    Create numericpattern119.py

---
+++ Numeric Patterns/numericpattern119.py
@@ -0,0 +1,38 @@
+height = int(input())
+c = 1
+
+for i in range(1,height-1):
+
+    for j in range(1,i+1):
+
+        if(c <= 9):
+            print("",c,end=" ")
+        
+        else:
+            print(c,end=" ")
+        c += 1
+
+    print()
+
+for i in range(height-1,0,-1):
+
+    for j in range(height,height-i,-1):
+        if(c <= 9):
+            print("",c,end=" ")
+        
+        else:
+            print(c,end=" ")
+        c += 1
+
+    print()
+
+# Sample Input :- 5
+# Output :-
+#  1 
+#  2  3 
+#  4  5  6 
+#  7  8  9 10 
+# 11 12 13 
+# 14 15 
+# 16 
+
