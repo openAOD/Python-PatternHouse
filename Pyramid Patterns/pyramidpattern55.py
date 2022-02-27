6b865f75893c219e135508c0e54fa70ece358611
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-02-27 21:06:57 +0530

    Create pyramidpattern55.py

---
+++ Pyramid Patterns/pyramidpattern55.py
@@ -0,0 +1,31 @@
+height = int(input())
+
+for i in range(1,height):
+
+    for j in range(height-i,1,-1):
+        print(end="  ")
+
+    for j in range(height-i-1,height-1):
+        print(j,end=" ")
+
+    print()
+
+for i in range(1,height):
+
+    for j in range(1,i+1):
+        print(end="  ")
+
+    for j in range(height-i,1,-1):
+        print(height-j,end=" ")
+
+    print()
+
+# Sample Input :- 5
+# Output :-
+#       3 
+#     2 3 
+#   1 2 3 
+# 0 1 2 3 
+#   1 2 3 
+#     2 3 
+#       3 
