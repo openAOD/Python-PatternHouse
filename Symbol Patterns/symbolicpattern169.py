1e60d998a6b64f073daf7a90613c6fd1bbfab4a8
Vipin Chaudhary <vipinchaudhary31122002@gmail.com>
2022-08-25 19:46:52 +0530

    deleted extra code

--- Symbol Patterns/symbolicpattern169.py
+++ Symbol Patterns/symbolicpattern169.py
@@ -9,25 +9,3 @@ for i in range(max):
     print(end='\n')
 for i in range(n):
     print([i+1],end='')
-
-
-# #include <stdio.h>
-
-# int main()
-# {
-#     int n = 5, max = 5;
-#     int a[] = {4, 3, 5, 1, 2};
-#     for(int i = 0; i < max; i++) {
-#         for(int j = 0; j < n; j++) {
-#             if(a[j] >= max - i)
-#                 printf(" # ");
-#             else
-#                 printf("   ");
-#         }
-#         printf("\n");
-#     }
-#     for(int i = 0; i < n; i++)
-#         printf("[%d]", i + 1);
-#     return 0;
-# }
-         
