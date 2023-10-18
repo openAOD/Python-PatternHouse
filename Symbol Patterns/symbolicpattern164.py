c912837c76f9690e0cb574d9814fec1b840acf04
JustAnAverageGuy <oceanic.arnav@gmail.com>
2023-10-18 20:04:39 +0530

    Add Symbolic pattern 164

---
+++ Symbol Patterns/symbolicpattern164.py
@@ -0,0 +1,16 @@
+try:
+    w = int(input("Number of chars in first line (default `5`): "))
+    c = input("Enter the character for the pattern (default `*`): ")
+except ValueError:
+    print("!! Invalid input, defaults are being used !!")
+    w = 5
+
+
+if len(c) == 0: c = '*'
+
+def gen_pattern_164(w:int,char:str) -> str:
+    s = [(char * i).rjust(w,' ') for i in range(w,0,-2)]
+    s += s[-2::-1]
+    return "\n".join(s)
+
+print(gen_pattern_164(w,c))
